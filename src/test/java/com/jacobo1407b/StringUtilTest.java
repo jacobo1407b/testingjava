7ffa189bbc7f0978c5eb03619b36843f8d238a44
Jacobo Rodrigo Hernandez Mendieta <jacobohernandezmendieta7@gmail.com>
2021-06-15 21:23:24 -0500

    excepcion

--- src/test/java/com/jacobo1407b/StringUtilTest.java
+++ src/test/java/com/jacobo1407b/StringUtilTest.java
@@ -4,26 +4,25 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class StringUtilTest {
-    @Test
-    public  void testRepeat1(){
+    public static void main(String[] args) {
+        assertEquals(StringUtil.repeat("hola",3), "holaholahola");
 
-        Assert.assertEquals(StringUtil.repeat("hola",1),"hola","hola");
+        if(!StringUtil.repeat("hola",3).equals("holaholahola")){
+            throw new RuntimeException("ERROR");
+        }else {
+            System.out.println("Todo Bien");
+        }
+
+        String result2 = StringUtil.repeat("hola",1);
+        if(!result2.equals("hola")){
+            throw new RuntimeException("ERROR");
+        }else
+            System.out.println("Todo Bien");
     }
 
-    @Test
-    public  void testRepeat3(){
-
-        Assert.assertEquals(StringUtil.repeat("hola",3), "holaholahola","holahola");
+    private static void assertEquals(String actual, String expected) {
+        if(!actual.equals(expected)){
+            throw new RuntimeException( actual + "No es igual al valor esperado");
+        }
     }
-
-    @Test
-    public void testRepat0(){
-        Assert.assertEquals(StringUtil.repeat("hola",0),"","");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testRepatNegativo(){
-        StringUtil.repeat("hola",-1);
-    }
-
 }
